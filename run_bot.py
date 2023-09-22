19bc315dc0e8c1b1789b97c1571b268f7fe7df29
Staid123 <pavlenkokirill568@gmail.com>
2023-09-22 17:42:36 +0300

    Небольшие изменения

--- run_bot.py
+++ run_bot.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import asyncpg
 
 from redis.asyncio import Redis
 from src.telegram import config
@@ -9,25 +8,23 @@ from src.postgres.utils import creating_tables
 from src.telegram import start_bot
 
 
-async def main():
+async def main() -> None:
     pool = await create_postgres_pool(config)
     redis = Redis(
         host=config.redis.host,
         db=config.redis.database,
         port = config.redis.port,
-        password = config.redis.password
+        #password = config.redis.password
     )
-
     try:
         async with pool.acquire() as connect:
             await creating_tables(connect, force=False)
-        await start_bot(config, pool, redis)
+        await start_bot(pool, redis, config)
     except Exception as e:
         raise e
     finally:
         await pool.close()
         await redis.close()
-
-
+ 
 logging.basicConfig(level=logging.DEBUG)
 asyncio.run(main())
