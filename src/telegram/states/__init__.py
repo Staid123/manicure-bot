9971a6022067f3dd7c8d250f376c3ddb003d3eb5
Staid123 <pavlenkokirill568@gmail.com>
2023-09-22 17:38:18 +0300

    Added

---
+++ src/telegram/states/__init__.py
@@ -0,0 +1,4 @@
+from src.telegram.states.admin_state import FSMadmin
+
+
+__all__ = (FSMadmin, )
---
+++ src/telegram/utils/__init__.py
@@ -0,0 +1,12 @@
+from src.telegram.utils.check_phone import check_phone
+from src.telegram.utils.get_year import get_year
+from src.telegram.utils.day_in_months import day_in_months
+from src.telegram.utils.working_time import working_time_in_day
+from src.telegram.utils.unpack_records import unpack_records
+from src.telegram.utils.format_text import get_text
+from src.telegram.utils.get_datetime import get_datetime
+
+__all__ = (check_phone, get_year, 
+           day_in_months, working_time_in_day, 
+           unpack_records, get_text,
+           get_datetime)
