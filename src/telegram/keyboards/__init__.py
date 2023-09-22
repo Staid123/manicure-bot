f0782a4a294ce67232b81640a812dc8d498f3c2e
Staid123 <pavlenkokirill568@gmail.com>
2023-09-22 17:33:54 +0300

    Added

---
+++ src/telegram/keyboards/__init__.py
@@ -0,0 +1,7 @@
+from src.telegram.keyboards.inline import answer_yes, year, month, day, time, service, delete_record
+from src.telegram.keyboards.admin_main_menu import set_main_menu
+
+
+__all__ = (answer_yes, set_main_menu, 
+           year, month, day, time, 
+           service, delete_record)
