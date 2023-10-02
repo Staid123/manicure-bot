1e9ad3ef9dd0a12fa7e686671ac8572518cd89fc
Staid123 <pavlenkokirill568@gmail.com>
2023-10-02 17:33:17 +0300

    Модель

--- src/core/dto/get_all_users.py
+++ src/core/dto/get_all_users.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
-
+from datetime import datetime
 
 @dataclass(frozen=True)
 class GetAllRecordsDTO:
-    client_name: str
     phone_number: str
-    datetime: str
+    datetime: datetime
     type_of_service: str
+    client_name: str | None = None
+    client_id: str | None = None
--- src/core/dto/insert_record.py
+++ src/core/dto/insert_record.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class InsertrecordDTO:
-    client_name: str
     phone_number: str
     type_of_service: str
     datetime: str
+    client_name: str | None = None
+    client_id: str | None = None
