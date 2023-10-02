4dd480bba08555ca9810698f08d96fbd36bbb9ce
Staid123 <pavlenkokirill568@gmail.com>
2023-10-02 17:32:09 +0300

    Файлы для форматирования дат

--- src/telegram/utils/day_in_months.py
+++ src/telegram/utils/day_in_months.py
@@ -1,6 +1,8 @@
 import calendar 
+from datetime import datetime
 
 
 def day_in_months(year: int, month: int) -> list:
     calend = calendar.Calendar().itermonthdays(int(year), month + 1)
-    return map(lambda date: '-' if date == 0 else str(date), calend)
+    return map(lambda date: '-' if not date else str(date), calend)
+
--- src/telegram/utils/get_year.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-
-def get_year() -> int:
-    return datetime.now().year
