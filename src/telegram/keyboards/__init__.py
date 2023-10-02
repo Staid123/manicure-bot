d17c760ba30bccf8b4c915ada0b0db8ca8cf6d4d
Staid123 <pavlenkokirill568@gmail.com>
2023-10-02 17:34:17 +0300

    init

--- src/telegram/keyboards/__init__.py
+++ src/telegram/keyboards/__init__.py
@@ -1,7 +1,9 @@
 from src.telegram.keyboards.inline import answer_yes, year, month, day, time, service, delete_record
 from src.telegram.keyboards.admin_main_menu import set_main_menu
+from src.telegram.keyboards.user_kb import requert_number
 
 
 __all__ = (answer_yes, set_main_menu, 
            year, month, day, time, 
-           service, delete_record)
+           service, delete_record, 
+           requert_number)
