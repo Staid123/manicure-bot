5c660b07855700e3b1de39c94137952c68057130
Staid123 <pavlenkokirill568@gmail.com>
2023-10-14 15:03:31 +0300

    Добавлены отправка сообщений админу о завершении отправки напоминаний клиентам

--- src/scheduler/newsletter/send_message.py
+++ src/scheduler/newsletter/send_message.py
@@ -2,18 +2,21 @@ from aiogram import Bot
 from fluentogram import TranslatorRunner
 from src.postgres.dao import DAO
 from datetime import datetime
-
+from src.telegram import Config
 
 async def send_message(
     dao: DAO,
     l10n: TranslatorRunner,
-    bot: Bot
+    bot: Bot,
+    config: Config
 ) -> None:
+    for admin_id in config.admin.ids:
+        await bot.send_message(
+            chat_id=admin_id,
+            text=l10n.admin.ready_reminder()
+        )
     records = await dao.user.get_all_records()
-    print(records)
     for user in records:
-        print(user)
-        print((user.datetime - datetime.now()).days)
         if (user.datetime - datetime.now()).days in (0, 1):
             await bot.send_message(
                 chat_id=user.client_id, 
@@ -22,3 +25,7 @@ async def send_message(
                     date=user.datetime
                 )
             )
+    for admin_id in config.admin.ids:
+        await bot.send_message(
+            chat_id=admin_id,
+            text=l10n.send.reminder.done())
