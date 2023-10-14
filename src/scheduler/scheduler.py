19892bf59e1a11dba2d5543c15b05f71406c5361
Staid123 <pavlenkokirill568@gmail.com>
2023-10-14 15:04:05 +0300

    Добавлена новая задача delete_records для удаления записей раз в день

--- src/scheduler/scheduler.py
+++ src/scheduler/scheduler.py
@@ -1,21 +1,28 @@
-from src.scheduler.newsletter import send_message
+from src.scheduler.newsletter import send_message, delete_records
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram import Bot
 from src.postgres.dao import DAO
 from asyncpg import Pool
 from fluentogram import TranslatorRunner
-
+from src.telegram import Config
 
 async def set_scheduled_jobs(
     scheduler: AsyncIOScheduler, 
     bot: Bot, 
     pool: Pool, 
-    l10n: TranslatorRunner
+    l10n: TranslatorRunner,
+    config: Config
 ):
     dao: DAO = DAO(pool)
     scheduler.add_job(
         send_message, 
         trigger="interval", 
-        seconds=3, 
-        args=(dao, l10n, bot)
+        days=1, 
+        args=(dao, l10n, bot, config)
+    )
+    scheduler.add_job(
+        delete_records,
+        trigger="interval",
+        days=1.5,
+        args=(dao, l10n, bot, config)
     )
